42bf17c560f08bc50c40d9a04035fe5cafe4b5b3
U-USERSAD\cmartima <cmartima@BOG-PF05KYPA.usersad.everis.int>
2015-11-11 10:24:46 -0500

    provision composer and codeception

--- vagrant/pre-puppet.sh
+++ vagrant/pre-puppet.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
 # pre-puppet.sh: Add your shell commands here
+sudo yum install -y curl
+sudo yum install -y wget
+sudo yum install -y vim
+
+#Composer
+curl -sS https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
+sudo chmod +x /usr/local/bin/composer
+
+#Codeception
+wget http://codeception.com/releases/1.8.7/codecept.phar .
+mv codecept.phar /usr/local/bin/codecept
+sudo chmod +x /usr/local/bin/codecept
